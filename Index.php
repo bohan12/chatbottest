daf7ce53c17a987104e6cc8e053151ed29845fc8
bohan12 <Wang670792079@gmail.com>
2019-08-04 15:01:58 +1200

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,6 @@
-<iframe height="430" width="350" src="https://bot.dialogflow.com/05eb396c-9e88-4f91-b731-709278ba2ec3"></iframe>
+
+<html>
+<?php
+ <iframe height="430" width="350" src="https://bot.dialogflow.com/05eb396c-9e88-4f91-b731-709278ba2ec3"></iframe>
+?>
+</html>
