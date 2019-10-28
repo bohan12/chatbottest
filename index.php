557058d0130a598e4ffcc10bc87bb46d27bd377a
Bohan <Wang670792079@gmail.com>
2019-10-28 22:24:04 +1300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <html>
-<iframe height="900" width="850" src="https://bot.dialogflow.com/05eb396c-9e88-4f91-b731-709278ba2ec3"></iframe>
+<iframe height="450" width="300" src="https://bot.dialogflow.com/05eb396c-9e88-4f91-b731-709278ba2ec3"></iframe>
 </html>
